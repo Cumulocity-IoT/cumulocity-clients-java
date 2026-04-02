5dc52f7730440f8b97d9992aa66bae5d8517c258
Tomasz Jegorow <tomasz.jegorow@cumulocity.com>
2026-04-02 13:14:44 +0200

    MTM-64359 Remove the MQTT Service Java client SDK

--- mqtt-service/websocket/src/main/java/com/cumulocity/mqtt/service/sdk/publisher/PublisherFactory.java
+++
@@ -1,8 +0,0 @@
-package com.cumulocity.mqtt.service.sdk.publisher;
-
-@Deprecated(forRemoval = true)
-public interface PublisherFactory {
-
-    Publisher build(PublisherConfig config);
-
-}
--- mqtt-service/websocket/src/main/java/com/cumulocity/mqtt/service/sdk/subscriber/SubscriberFactory.java
+++
@@ -1,8 +0,0 @@
-package com.cumulocity.mqtt.service.sdk.subscriber;
-
-@Deprecated(forRemoval = true)
-public interface SubscriberFactory {
-
-    Subscriber build(SubscriberConfig config);
-
-}
